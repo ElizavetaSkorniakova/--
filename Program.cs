b5e3a837c2676ecfa5077916b1b4b1fc6bf4c5a8
elizaveta <elizavetasesina@mail>
2023-03-26 21:46:13 +0300

    Program created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+string[] EnterUserArray (int arrayLength) // функция ввода записей в массив пользователем
+{
+string[] userArray = new string[arrayLength];
+int i = 0;
+ while (i < arrayLength)
+  {
+  Console.WriteLine("Введите запись");
+  userArray[i] = Console.ReadLine();
+  i++;
+  }
+ return userArray;
+}
+
+string[] analyzedUserArray (string[] userArray)
+{
+   int GreaterThan0Counter = 0;
+  for ( int i = 0; i < userArray.Length;  i++ )
+  {
+    if (userArray[i] > 0) GreaterThan0Counter++;
+  }
+  Console.WriteLine();
+  Console.WriteLine("количество введеных чисел больше 0 = " + GreaterThan0Counter);
+  return GreaterThan0Counter;
+}
+
+int G = getUserData("Введите количество записей в массиве");
+string[] arrayDisplayG = EnterUserArray (G);
